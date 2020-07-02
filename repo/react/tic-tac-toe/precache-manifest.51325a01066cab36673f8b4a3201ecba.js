a2afaabe82ebff3a1ecc7730816d4878fc0fab53
willarchen <willarchen@163.com>
2020-07-02 12:16:18 +0800

    feat(repo/react): add tic-tac-toe game

---
+++ repo/react/tic-tac-toe/precache-manifest.51325a01066cab36673f8b4a3201ecba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970e9fe3e7ba8433c206e1363364febe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcb5ebd301bd6a9ce778",
+    "url": "/static/css/main.1d7414a3.chunk.css"
+  },
+  {
+    "revision": "185550ba4c511e915492",
+    "url": "/static/js/2.e977bf79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e977bf79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcb5ebd301bd6a9ce778",
+    "url": "/static/js/main.7f83917c.chunk.js"
+  },
+  {
+    "revision": "828c0bdfa56a628b17a7",
+    "url": "/static/js/runtime-main.eb9be228.js"
+  }
+]);
